f77d077019b8e27cf9d0b80a5a34491cb9a86cde
Jathin Devaiah <devaiah.jathin@gmail.com>
2025-02-17 14:43:32 -0500

    Adding path util for fetching root directory

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const roorDir = require('../util/path');
 
 router.get('/addProduct', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(roorDir, 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
