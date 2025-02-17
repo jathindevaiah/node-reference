f7d7470553b1b930672a77a1bbb2a0c52804ec4a
Jathin Devaiah <devaiah.jathin@gmail.com>
2025-02-17 14:26:00 -0500

    Adding 404 response for no match

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
